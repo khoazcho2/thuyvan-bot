b2e0940e7678119a199f54b23e7fa46055b4a1c8
khoazcho2 <vot835848@gmail.com>
2026-03-01 17:04:45 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.getenv("BOT_TOKEN", "8643690918:AAF2cclKGfyECczdoQv1hvgAQ1Ym-nOicsE")
+
+# ID của người tạo bot (người duy nhất dùng được lệnh /banglobal)
+OWNER_ID = int(os.getenv("OWNER_ID", "8337495954"))
