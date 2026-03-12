de569e502696f4cb1c6753a57cde254bb20c8ca2
khoazcho2 <vot835848@gmail.com>
2026-03-12 18:18:30 +0700

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-TOKEN = os.getenv("BOT_TOKEN", "8643690918:AAEN1ENqPWmHhovnlgtEFWDtvEctX4zzm4k").strip()
-
-# ID của người tạo bot (người duy nhất dùng được lệnh /banglobal)
-OWNER_ID = int(os.getenv("OWNER_ID", "8337495954").strip() or "0")
