8245e5871dec901867fec8cd135c4eef6fc521f9
khoazcho2 <vot835848@gmail.com>
2026-03-12 17:51:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-TOKEN = os.getenv("BOT_TOKEN", "8643690918:AAEN1ENqPWmHhovnlgtEFWDtvEctX4zzm4k")
+TOKEN = os.getenv("BOT_TOKEN", "").strip()
+
 
 # ID của người tạo bot (người duy nhất dùng được lệnh /banglobal)
 OWNER_ID = int(os.getenv("OWNER_ID", "8337495954"))
